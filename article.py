77a7ffd6c47dd391e7ddc1181542d70a1a86f88d
Andrew Green <agreen@ebi.ac.uk>
2023-05-30 19:54:48 +0100

    Make some getters for the article

--- article.py
+++ article.py
@@ -9,3 +9,32 @@ class Article:
 
     def __str__(self):
         return f"Title: {self.title}\nAuthor List: {self.author_list}\nAbstract: {self.abstract}\nDate: {self.date}\nSections: {self.sections}\nType: {self.type}"
+
+    def __repr__(self):
+        return f"Title: {self.title}\nAuthor List: {self.author_list}\nAbstract: {self.abstract}\nDate: {self.date}\nSections: {self.sections}\nType: {self.type}"
+
+    def get_title(self):
+        return self.title
+
+    def get_author_list(self):
+        return self.author_list
+
+    def get_abstract(self):
+        return self.abstract
+
+    def get_date(self):
+        return self.date
+
+    def get_sections(self):
+        return self.sections
+
+    def get_type(self):
+        return self.type
+
+    def get_body(self):
+        body_str = ""
+        for key, value in self.sections.items():
+            body_str += key + "\n"
+            body_str += value + "\n\n"
+
+        return body_str
